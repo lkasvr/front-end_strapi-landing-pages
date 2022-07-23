353c5c46930652789db54650138d796bd0778716
Lucas Vieira <lkasvr@outlook.com>
2022-07-23 19:27:40 -0300

    Criação do Componente NavLinks e teste, mais ajustes de Snapshots

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -9,86 +9,17 @@ describe('< />', () => {
   });
 
   it('should match snapshot', () => {
-    const container = renderTheme(<TextComponent>Children</TextComponent>);
-    expect(container).toMatchInlineSnapshot(`
-      Object {
-        "asFragment": [Function],
-        "baseElement": .c0 {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
         font-size: 2.4rem;
       }
 
-      <body>
-          <div>
-            <p
-              class="c0"
-            >
-              Children
-            </p>
-          </div>
-        </body>,
-        "container": .c0 {
-        font-size: 2.4rem;
-      }
-
-      <div>
-          <p
-            class="c0"
-          >
-            Children
-          </p>
-        </div>,
-        "debug": [Function],
-        "findAllByAltText": [Function],
-        "findAllByDisplayValue": [Function],
-        "findAllByLabelText": [Function],
-        "findAllByPlaceholderText": [Function],
-        "findAllByRole": [Function],
-        "findAllByTestId": [Function],
-        "findAllByText": [Function],
-        "findAllByTitle": [Function],
-        "findByAltText": [Function],
-        "findByDisplayValue": [Function],
-        "findByLabelText": [Function],
-        "findByPlaceholderText": [Function],
-        "findByRole": [Function],
-        "findByTestId": [Function],
-        "findByText": [Function],
-        "findByTitle": [Function],
-        "getAllByAltText": [Function],
-        "getAllByDisplayValue": [Function],
-        "getAllByLabelText": [Function],
-        "getAllByPlaceholderText": [Function],
-        "getAllByRole": [Function],
-        "getAllByTestId": [Function],
-        "getAllByText": [Function],
-        "getAllByTitle": [Function],
-        "getByAltText": [Function],
-        "getByDisplayValue": [Function],
-        "getByLabelText": [Function],
-        "getByPlaceholderText": [Function],
-        "getByRole": [Function],
-        "getByTestId": [Function],
-        "getByText": [Function],
-        "getByTitle": [Function],
-        "queryAllByAltText": [Function],
-        "queryAllByDisplayValue": [Function],
-        "queryAllByLabelText": [Function],
-        "queryAllByPlaceholderText": [Function],
-        "queryAllByRole": [Function],
-        "queryAllByTestId": [Function],
-        "queryAllByText": [Function],
-        "queryAllByTitle": [Function],
-        "queryByAltText": [Function],
-        "queryByDisplayValue": [Function],
-        "queryByLabelText": [Function],
-        "queryByPlaceholderText": [Function],
-        "queryByRole": [Function],
-        "queryByTestId": [Function],
-        "queryByText": [Function],
-        "queryByTitle": [Function],
-        "rerender": [Function],
-        "unmount": [Function],
-      }
+      <p
+        class="c0"
+      >
+        Children
+      </p>
     `);
   });
 });

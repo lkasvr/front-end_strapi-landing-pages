c22bd1cb78a5fa967ef8fc40093ba0d65424f274
Lucas Vieira <lkasvr@outlook.com>
2022-07-25 19:16:40 -0300

    Testes Componentes SectionBackground e SectionContainer

--- src/components/SectionContainer/SectionContainer.test.jsx
+++ src/components/SectionContainer/SectionContainer.test.jsx
@@ -4,7 +4,12 @@ import { SectionContainer } from '.';
 
 describe('<SectionContainer />', () => {
   it('should render', () => {
-    renderTheme(<SectionContainer>Children</SectionContainer>);
-    expect(screen.getByRole('SectionContainer')).toBeInTheDocument();
+    const { container } = renderTheme(
+      <SectionContainer>
+        <h1>Children</h1>
+      </SectionContainer>,
+    );
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });

6518e6b38eb5eda04cd3ea5590459536384211bf
Lucas Vieira <lkasvr@outlook.com>
2022-07-28 17:46:46 -0300

    Componente GridContent e Testes

---
+++ src/components/GridContent/index.jsx
@@ -0,0 +1,26 @@
+import P from 'prop-types';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+import { SectionBackground } from '../SectionBackground';
+import * as Styled from './styles';
+
+export const GridContent = ({ title, html, background }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridContent.propTypes = {
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};

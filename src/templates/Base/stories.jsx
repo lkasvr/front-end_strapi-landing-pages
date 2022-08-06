0f699c739f25c7a04d6bc69395e97ecc67b75985
Lucas Vieira <lkasvr@outlook.com>
2022-08-06 18:24:11 -0300

    Testes Base Template

--- src/templates/Base/stories.jsx
+++ src/templates/Base/stories.jsx
@@ -1,22 +1,6 @@
 import { Base } from '.';
 
-import mock from './mock';
-import { GridText } from '../../components/GridText';
-import gridMock from '../../components/GridText/mock';
-
-export const mockBase = {
-  children: (
-    <>
-      <GridText {...gridMock} background />
-      <GridText {...gridMock} />
-      <GridText {...gridMock} background />
-      <GridText {...gridMock} />
-      <GridText {...gridMock} background />
-      <GridText {...gridMock} />
-    </>
-  ),
-  ...mock,
-};
+import { mockBase } from './mock';
 
 export default {
   title: 'Templates/Base',
